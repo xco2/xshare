68e53206c2029d48a639ee5ffafbc8c455ae68ed
Yong Git <1013588891@qq.com>
2022-08-08 17:23:42 +0800

    feat: init

---
+++ web/src/views/Home/Hearder.vue
@@ -0,0 +1,24 @@
+<template>
+  <img src="https://vuejs.org/images/logo.png" alt="vue logo" />
+  <div class="header">{{ title ?? '-' }}</div>
+  <!-- <div class="header" v-boolean="boolean">test</div> -->
+</template>
+
+<script lang="ts" setup>
+  defineProps<{
+    title?: string;
+  }>();
+</script>
+
+<style scoped lang="less">
+  img {
+    width: 300px;
+    height: 300px;
+  }
+
+  .header {
+    font-size: 64px;
+    font-weight: 600;
+    text-align: center;
+  }
+</style>
---
+++ web/src/views/Home/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home">
+    <Hearder :title="title" />
+    <div class="text-6xl font-bold mt-10 text-green-500">Hello Tailwindcss</div>
+    <div class="text-6xl font-bold mt-10 text-yellow-300">{{ storeTitle }}</div>
+    <div class="text-6xl font-bold mt-10 text-blue-400">{{ newCount }}</div>
+    <div class="text-6xl font-bold mt-10 text-red-500">Go!</div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Home',
+  };
+</script>
+
+<script lang="ts" setup>
+  import { useHomeStore } from '@/store/modules/home';
+  import { storeToRefs } from 'pinia';
+  import Hearder from './Hearder.vue';
+
+  const title = ref(`Vite-Vue3-TypeScript`);
+
+  /**
+   * 使用vue内置全局环境变量
+   */
+  console.log(`${import.meta.env.VITE_APP_ENV}`);
+
+  /**
+   * 发送网络请求
+   */
+  // import { useRequest } from 'vue3-hooks-plus';
+  // import { getName } from './services';
+  // 请求实例
+  // const { data } = useRequest(() => getName(123));
+
+  /**
+   * 使用pinia状态管理
+   */
+  const store = useHomeStore();
+  const storeTitle = computed(() => store.getFuncTitle);
+  // 保持响应式
+  const { count } = storeToRefs(store);
+
+  const newCount = computed(() => `state ${count.value}`);
+  // store.$patch({ count: store.count + 10 });
+  // store.incrementPromise();
+</script>
+
+<style scoped lang="less">
+  .home {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+</style>
