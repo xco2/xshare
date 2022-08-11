67a0ec6a56ef89775029174d977e74ea45028698
xco2 <627759118@qq.com>
2022-08-11 15:44:24 +0800

    极简界面版

---
+++ service/nohup.sh
@@ -0,0 +1,6 @@
+#source /home/opt/cychome/py36_conda/bin/activate
+cd /home/xco2/xshare/service
+#kill -9 $(ps aux | grep 'cyc_YunFu.py' | awk '{print $2}')
+nohup python upload.py >> /home/xco2/xshare/service/flask.log 2>&1 &
+#bash /home/opt/cychome/PyFlaskYunFu/nohup_update.sh
+#tail -f /home/opt/cychome/PyFlaskYunFu.log
