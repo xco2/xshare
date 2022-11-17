a7a0933fd9078705f3428cc9affd1d31bf2061ea
YongGit <1013588891@qq.com>
2022-11-17 13:37:01 +0800

    feat: access

--- web/src/views/user-upload/Upload.vue
+++ web/src/views/user-upload/Upload.vue
@@ -1,48 +1,69 @@
 <template>
   <a-upload
-    v-model:file-list="fileList"
-    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-    @change="handleChange"
+    :file-list="fileList"
+    :before-upload="beforeUpload"
+    @remove="handleRemove"
+    :max-count="1"
   >
     <a-button type="primary" shape="round" size="large" style="display: flex; align-items: center">
       <template #icon>
         <UploadOutlined />
       </template>
-      上传
+      选择文件
     </a-button>
   </a-upload>
 </template>
 <script setup lang="ts">
   import { UploadOutlined } from '@ant-design/icons-vue'
   import { ref } from 'vue'
-  import type { UploadChangeParam, UploadProps } from 'ant-design-vue'
+  import { message, UploadProps } from 'ant-design-vue'
+  import 'ant-design-vue/es/message/style/index'
 
-  const fileList = ref<UploadProps['fileList']>([
-    // {
-    //   uid: '1',
-    //   name: 'xxx.png',
-    //   status: 'done',
-    //   response: 'Server Error 500', // custom error message to show
-    //   url: 'http://www.baidu.com/xxx.png',
-    // },
-    // {
-    //   uid: '2',
-    //   name: 'yyy.png',
-    //   status: 'done',
-    //   url: 'http://www.baidu.com/yyy.png',
-    // },
-    // {
-    //   uid: '3',
-    //   name: 'zzz.png',
-    //   status: 'error',
-    //   response: 'Server Error 500', // custom error message to show
-    //   url: 'http://www.baidu.com/zzz.png',
-    // },
-  ])
+  const fileList = ref<UploadProps['fileList']>([])
+  // const uploading = ref<boolean>(false)
 
-  const handleChange = ({ file, fileList }: UploadChangeParam) => {
-    if (file.status !== 'uploading') {
-      console.log(file, fileList)
+  const beforeUpload: UploadProps['beforeUpload'] = (file) => {
+    if (fileList.value && fileList.value?.length < 1) {
+      fileList.value = [...(fileList.value ?? []), file]
+    } else {
+      console.log(777)
+
+      message.error('只能上传一个文件')
+    }
+
+    return false
+  }
+
+  const handleRemove: UploadProps['onRemove'] = (file) => {
+    const index = fileList.value?.indexOf(file)
+    const newFileList = fileList.value?.slice()
+    if (index !== undefined && newFileList !== undefined) {
+      newFileList.splice(index, 1)
+      fileList.value = newFileList
     }
   }
+
+  // const handleUpload = () => {
+  //   const formData = new FormData()
+  //   // @ts-ignore
+  //   fileList.value?.forEach((file: UploadProps['fileList'][number]) => {
+  //     formData.append('upload_file', file as any)
+  //   })
+  //   uploading.value = true
+
+  //   // You can use any AJAX library you like
+  //   // request('https://www.mocky.io/v2/5cc8019d300000980a055e76', {
+  //   //   method: 'post',
+  //   //   data: formData,
+  //   // })
+  //   //   .then(() => {
+  //   //     fileList.value = []
+  //   //     uploading.value = false
+  //   //     message.success('upload successfully.')
+  //   //   })
+  //   //   .catch(() => {
+  //   //     uploading.value = false
+  //   //     message.error('upload failed.')
+  //   //   })
+  // }
 </script>
